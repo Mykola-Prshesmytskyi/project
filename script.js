a309cedf6468b88c7d78378da8969217e005cf48
Mykola <Kolyaskokio@gmail.com>
2022-08-11 17:00:46 +0300

    one

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-"use strict";
+const numberOfFilms = +prompt('кіко?', '');
 
-const category = 'toys';
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 
+};
+
+const a = prompt('який?', ''),
+      b = prompt('Файний?', ''),
+      c = prompt('який?', ''),
+      d = prompt('Файний?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
